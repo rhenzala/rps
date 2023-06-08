e2685e71a1f1eceb42a71e3731e10fd585089e94
zal626 <97076128+zal626@users.noreply.github.com>
2023-06-08 21:30:54 +0800

    Add the playGame function that plays 5 rounds of game

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function getComputerChoice(){
-    let choices = ["r", "p", "s"];
+    let choices = ["Rock", "Paper", "Scissor"];
     let pick = choices[Math.floor(Math.random()*choices.length)];
     return pick;
 }
@@ -11,35 +11,56 @@ function playRound(playerSelection, computerSelection){
     if (player == computer){
         return "tie";
     }
-    else if (player == "r"){
-        if (computer == "p"){
+    else if (player == "rock"){
+        if (computer == "paper"){
             return "com";
         } 
-        else if (computer == "s"){
+        else if (computer == "scissor"){
             return "player"
         }
     }
-    else if (player == "p"){
-        if (computer == "s"){
+    else if (player == "paper"){
+        if (computer == "scissor"){
             return "com";
         } 
-        else if (computer == "r"){
+        else if (computer == "rock"){
             return "player"
         }
     }
-    else if (player == "s"){
-        if (computer == "r"){
+    else if (player == "scissor"){
+        if (computer == "rock"){
             return "com";
         } 
-        else if (computer == "p"){
+        else if (computer == "paper"){
             return "player"
         }
     }
 }
-const p = 'r';
-const c = getComputerChoice();
-console.log(playRound(p, c));
 
 function playGame(){
-
+    let playerScore = 0;
+    let comScore = 0;
+    for (let i = 0; i < 5; i++){
+        const playerPick = prompt("Rock, Paper, Scissor");
+        const comPick = getComputerChoice();
+        let game = playRound(playerPick, comPick);
+        console.log(`Round ${i+1}`);
+        if (game == "tie"){
+            console.log("It's a tie!");
+        }
+        else if (game == "player"){
+            console.log(`You win! ${playerPick} beats ${comPick}`);
+            playerScore += 1;
+        }
+        else if (game == "com"){
+            console.log(`You lose! ${comPick} beats ${playerPick}`)
+            comScore += 1;
+        }
+        else{
+            console.log("ERROR: Could not play game properly.")
+        }
+    }
+    console.log(`Player: ${playerScore} | Computer: ${comScore}`)
 }
+
+playGame();
